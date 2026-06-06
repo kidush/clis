7e65e58c4b32d257b53f5d4996b8dc368f9c6133
kidush <thiago.evandson@gmail.com>
2026-06-05 21:21:23 -0300

    Add flags -c and -w

--- wc.pl
+++ wc.pl
@@ -26,12 +26,31 @@ sub count_words {
     return $word_count;
 }
 
+sub count_chars {
+    my $file = shift;
+    open(my $fh, '<', $file) or die "Could not open file '$file': $!";
+    my $char_count = 0;
+    while (<$fh>) {
+        $char_count += length;
+    }
+    close($fh);
+    return $char_count;
+}
+
 if ($firstArg =~ /^-/) {
     my $file = $ARGV[1];
     if ($firstArg eq '-l') {
         my $line_count = count_lines($file);
 
         print "$line_count $file\n";
+    } elsif ($firstArg eq '-w') {
+        my $word_count = count_words($file);
+
+        print "$word_count $file\n";
+    } elsif ($firstArg eq '-c') {
+        my $char_count = count_chars($file);
+
+        print "$char_count $file\n";
     }
 } else {
     my $file = $firstArg;
@@ -43,6 +62,7 @@ if ($firstArg =~ /^-/) {
     open(my $fh, '<', $file) or die "Could not open file '$file': $!";
     my $line_count = count_lines($file);
     my $word_count = count_words($file);
+    my $char_count = count_chars($file);
 
-    print "$line_count $word_count $file\n";
+    print " $line_count  $word_count $char_count $file\n";
 }
